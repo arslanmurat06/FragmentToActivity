0ca48a00b2792a1dd255279eed507767792ee3b3
ideaman06 <mrtarsln06@gmail.com>
2017-05-17 13:57:02 +0300

    Fragment to Activity Communication

--- app/src/main/java/com/egitim/marstech/fragmentanlamak/MainActivity.java
+++ app/src/main/java/com/egitim/marstech/fragmentanlamak/MainActivity.java
@@ -1,13 +1,37 @@
 package com.egitim.marstech.fragmentanlamak;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ColorFragment.OnColorChangeListener {
+    ColorFragment colorFragment=new ColorFragment();
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout=(LinearLayout) findViewById(R.id.main_linearLayout);
+
+
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container,colorFragment)
+                .commit();
+
+
+    }
+
+    @Override
+    public void colorChanged(String colorName) {
+
+        if(colorName.equals("RED")) {linearLayout.setBackgroundColor(Color.RED);}
+        else if(colorName.equals("GREEN")) {linearLayout.setBackgroundColor(Color.GREEN);}
+        else if (colorName.equals("BLUE")) {linearLayout.setBackgroundColor(Color.BLUE);}
+
     }
 }
